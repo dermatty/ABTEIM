0107356f4477455abd160d5b30739af9032c035c
dermatty <stephan@untergrabner.at>
2025-05-08 10:23:08 +0200

    update

--- abteim/__init__.py
+++ abteim/__init__.py
@@ -3,14 +3,14 @@ from importlib.metadata import version
 import toml
 
 try:
-    with open("../pyproject.toml", mode="r") as config:
+    with open("pyproject.toml", mode="r") as config:
         toml_file = toml.load(config)
-    __version__ = toml_file["tool"]["poetry"]["version"]
+    __version__ = toml_file["project"]["version"]
     __appname__ = "abteim" + __version__.split(".")[0]
     __appabbr__ = "abt" + __version__.split(".")[0]
     __startmode__ = "dev"
-except Exception:
-    print("err")
+except Exception as e:
+    print("error: " + str(e))
     __startmode__ = "systemd"
     __appname__ = "abteim"
     __appabbr__ = "abt"
